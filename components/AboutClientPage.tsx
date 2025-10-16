91b9b29030be461b298eea6a343370dd49d476f5
Gideon <golaiya123@gmail.com>
2025-10-16 21:51:31 +0100

    minor changes

--- components/AboutClientPage.tsx
+++ components/AboutClientPage.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import AboutHero from './AboutHero'
 import AboutSection from './AboutSection'
--- components/ContactClientPage.tsx
+++ components/ContactClientPage.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import ContactMain from './ContactMain'
 import Cta from './Cta'
