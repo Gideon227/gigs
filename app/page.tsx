4dbc7a20444b9dabbdb46d3d32a47f6aeac9731f
Gideon <golaiya123@gmail.com>
2025-11-08 01:00:07 +0100

    minor fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,6 @@
-// import Image from "next/image";
-// import { redirect } from "next/navigation";
-
-// export default function Home() {
-//   redirect('/browse-jobs?country=United+States&page=1&limit=10')
-// }
-
-
-// /app/page.tsx
-'use client';
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace('/browse-jobs?country=United+States&page=1&limit=10');
-  }, [router]);
-
-  return null; // or a loading spinner
+  redirect('/browse-jobs?country=United+States&page=1&limit=10')
 }
