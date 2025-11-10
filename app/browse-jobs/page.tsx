43c63d875649a6137b480962b24ca9f38dbb766d
Gideon <golaiya123@gmail.com>
2025-11-11 00:01:12 +0100

    minor fixes

--- app/browse-jobs/page.tsx
+++ app/browse-jobs/page.tsx
@@ -32,15 +32,15 @@ const Jobs = () => {
         <div className='browse-jobs_bg w-full lg:h-[606px] max-lg:h-[350px] flex justify-center lg:px-20 max-lg:px-8 max-sm:px-4 lg:space-x-10 max-lg:space-x-4 max-sm:space-x-0'>
             <div className='max-w-[1600px] flex-1 flex items-center justify-center max-md:flex-col'>
                 <div className='md:w-2/3 max-sm:space-y-0 space-y-2 max-lg:space-y-3 lg:pr-6'>
-                    <div className='flex space-x-1 items-center'>
-                        <p className='text-sm leading-[24px] text-[#E3E3E3]'>Welcome to Gigs.Tech</p>
-                        <Image src='/waving.png' alt='waving emoji' width={21} height={21} placeholder="blur" className='flex items-center object-contain -mt-1 max-md:hidden'/>
-                        <Image src='/waving.png' alt='waving emoji' width={18} height={18} placeholder="blur" className='flex items-center object-contain -mt-1 md:hidden'/>
-                    </div>
+                  <div className='flex space-x-1 items-center'>
+                    <p className='text-sm leading-[24px] text-[#E3E3E3]'>Welcome to Gigs.Tech</p>
+                    <Image src='/waving.png' alt='waving emoji' width={21} height={21} placeholder="blur" className='flex items-center object-contain -mt-1 max-md:hidden'/>
+                    <Image src='/waving.png' alt='waving emoji' width={18} height={18} placeholder="blur" className='flex items-center object-contain -mt-1 md:hidden'/>
+                  </div>
 
-                    <h1 className='font-semibold text-[40px] lg:leading-[60px] text-heading max-lg:text-[27px] max-lg:leading-[32px] max-sm:text-[24px] max-sm:leading-8'>Power Platform & Dynamics <br /> 365 jobs all in one place.</h1>
+                  <h1 className='font-semibold text-[40px] lg:leading-[60px] text-heading max-lg:text-[27px] max-lg:leading-[32px] max-sm:text-[24px] max-sm:leading-8'> Explore Microsoft Power Platform & Dynamics 365 Jobs</h1>
                     
-                    <p className='text-heading text-[16px] leading-[27px] max-lg:leading-[25px] max-lg:text-[15px] max-sm:text-[13px] max-sm:leading-6 max-sm:pt-4'>Gigs.Tech is a hyper-focused job aggregation platform built from the ground up to serve<br className='max-md:hidden'/> one mission: Help Microsoft job seekers discover the freshest roles, and help recruiters<br className='max-md:hidden'/> connect with qualified candidates — faster and smarter. </p>
+                  <p className='text-heading text-[16px] leading-[27px] max-lg:leading-[25px] max-lg:text-[15px] max-sm:text-[13px] max-sm:leading-6 max-sm:pt-4 md:pr-6'>Gigs.Tech is a hyper-focused job aggregation platform built from the ground up to serve<br className='max-md:hidden'/> one mission: Help Microsoft job seekers discover the freshest roles, and help recruiters<br className='max-md:hidden'/> connect with qualified candidates — faster and smarter. </p>
                 </div>
                 <div className='md:w-1/3 flex items-end justify-center flex-1 max-md:hidden'>
                     <Image src='/browse-jobs_hero.svg' alt='About us page illustration' placeholder="blur" priority width={585} height={514} className='object-contain'/>
